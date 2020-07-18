427a6a3e3e15457c1be678f2d704d238bd3c29a8
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-07-17 22:32:13 -0700

    added dropdown for products

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {Menu, Icon} from "semantic-ui-react";
+import {Menu, Icon, Dropdown} from "semantic-ui-react";
 import Link from "next/link";
 
 export default function Navbar() {
@@ -10,7 +10,13 @@ export default function Navbar() {
                     <i className="hidden">home</i>
                 </Menu.Item>
             </Link>
-            <Link href="/"><Menu.Item>products</Menu.Item></Link>
+            <Dropdown text='Products' className="item">
+                <Dropdown.Menu>
+                    <Dropdown.Item text='pumps'/>
+                    <Dropdown.Item text='wound dressings'/>
+                    <Dropdown.Item text='canisters'/>
+                </Dropdown.Menu>
+            </Dropdown>
             <Link href="/"><Menu.Item>about</Menu.Item></Link>
             <Link href="/Contact"><Menu.Item>contact</Menu.Item></Link>
         </Menu>
