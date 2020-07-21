6d49474b9582d33612f56567ca670662087fccf5
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-07-21 15:59:37 -0700

    added lorem ipsum

--- pages/products/Pump.jsx
+++ pages/products/Pump.jsx
@@ -2,7 +2,7 @@ import Product from "../../components/product";
 
 export default function Pump() {
     return  <Product
-                title="pump"
-                description="wound pump"
+                title="Wound Pumps"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             />
 }
